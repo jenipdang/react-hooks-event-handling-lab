aaae90ed106227bb1a9d42a450820ff81c71b75b
Jeni Dang <jenipdang@yahoo.com>
2022-01-10 18:19:42 -0800

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+const EyesOnMe = () => {
+    const handleFocus = () => {
+        console.log('Good!')
+    }
+
+    const handleBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <button onFocus={handleFocus} onBlur={handleBlur}>
+            Eyes On Me
+        </button>
+    )
+
+}
+
+export default EyesOnMe
